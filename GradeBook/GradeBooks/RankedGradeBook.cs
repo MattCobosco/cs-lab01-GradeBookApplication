c757f43311b80affab8f733d1b69f1a5b14a2928
Mateusz Kalucki <mateuszkalucki@gmail.com>
2022-03-03 18:26:45 +0100

    GetLetterGrade in progress

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,22 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Not enough students (<5)");
+            
+            List<Student> StudentsSorted = Students.OrderByDescending(s => s.AverageGrade).ToList();
+            int oneFifth = (int)Math.Ceiling(StudentsSorted.Count *0.2);
+
+            if(StudentsSorted[oneFifth-1] <= averageGrade)
+            {
+
+            }
+
+                
         }
     }
 }
